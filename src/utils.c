bc2eedecbcef8cb1385b0dca8052055b9d915a98
Leonardo Molina <lmolina3@nd.edu>
2026-01-07 22:11:14 -0600

    feat: create utility functions for git

---
+++ src/utils.c
@@ -0,0 +1,57 @@
+/* utils.c: utility functions used by git clone */
+
+#include "utils.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdarg.h>
+#include <string.h>
+
+/* Functions */
+
+/**
+ * str_join Concatenates two strings into a new heap-allocated buffer.
+ * 
+ * @param s1 The first string (prefix).
+ * @param s2 The second string (suffix).
+ * @param ...   Additional strings to join. The list MUST end with NULL.
+ * @return A pointer to the new concatenated string, or NULL if memory fails.
+ * @note The caller MUST provide NULL as the last argument to signal the end.
+ * @note The caller is responsible for calling free() on the returned pointer.
+ */
+char *str_join(const char *s1, ...){
+    if (!s1) { return NULL; }
+
+    int total_len = strlen(s1);
+    int str_count = 1;
+
+    // find length to malloc 
+    va_list args;
+    va_start(args, s1);
+    const char *s = NULL;
+    while ((s = va_arg(args, const char *))){
+        total_len += strlen(s);
+        str_count++;
+    }
+    va_end(args);
+
+    char *str_joined = safe_calloc(sizeof(char), total_len + str_count);
+
+    // construct string 
+    size_t s_len = strlen(s1);
+    size_t offset = s_len;
+    memcpy(str_joined, s1, s_len);
+    
+    va_start(args, s1);
+    while ((s = va_arg(args, const char *))){
+        s_len = strlen(s);
+        if (s_len == 0) {continue;}
+        memcpy(str_joined + offset++, "/", 1);
+        memcpy(str_joined + offset, s, s_len);
+        offset += s_len;
+    }
+    va_end(args);
+
+    return str_joined;
+}
